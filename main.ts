7b90f5a1ecdf99bedf05f0a7a1fd01b3914780ca
NomanGhaziani <153339067+NomanGhaziani@users.noreply.github.com>
2024-03-23 01:14:57 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+let email : string ="92nomanattari@gmail.com"
+let pass :string ="123456"
+if(email =="92nomanattari@gmail.com" && pass =="123456"){
+    console.log('succesfull');
+}
+else{
+    console.log('Invailed');
+}
